8fc94191042d916b050e7e1fba8d4ad80280511e
ms29ash <ms29ash@gmail.com>
2022-05-18 22:39:15 +0530

    added loading

--- src/Components/Food.js
+++ src/Components/Food.js
@@ -1,49 +1,75 @@
-import React, { useRef, useContext, useEffect } from 'react'
+import React, { useRef } from 'react'
 import FoodCard from './FoodCard';
+import {
+    useQuery,
+} from 'react-query'
+import axios from '../axios'
+import tw from "tailwind-styled-components";
+import { BsCaretRightSquare, BsCaretLeftSquare } from "react-icons/bs";
+
 
 
 function Food() {
     const scrollRef = useRef(null);
 
-    useEffect(() => {
+    const fetchFood = async () => {
+        return axios.get('/food/food')
+    }
 
-    })
+    const { data: food, isLoading, isError, error } = useQuery('food', fetchFood)
+
+
 
 
 
     const rightScroll = () => {
-        scrollRef.current.scrollLeft += 200;
+        scrollRef.current.scrollLeft += 400;
 
     }
     const leftScroll = () => {
-        scrollRef.current.scrollLeft -= 200;
+        scrollRef.current.scrollLeft -= 400;
     }
     return (
         <>
-            <section className=" px-3 bg-white xl:px-20 pt-2 xl:pt-5 pb-10" id="food-section">
-                <h1 className=" text-orangefood headings text-center ">Get Your Food Here</h1>
+            <FoodSection id="food-section">
+                <Head >Get Your Food Here</Head>
                 <div className="flex justify-between w-screen z-20">
-                    <button className="hidden md:block backdrop-blur  absolute mt-48 z-20 ml-8 text-white  px-4 py-3 text-3xl bg-black rounded left-0 cursor-pointer hover:bg-grayfood" onClick={leftScroll}>&laquo;</button>
-                    <button className="hidden md:block backdrop-blur  absolute mt-48 z-20 ml-8 text-white  px-4 py-3 text-3xl bg-black rounded right-0 cursor-pointer hover:bg-grayfood" onClick={rightScroll}>&raquo;</button>
+                    <LeftBtn onClick={leftScroll} >
+                        <BsCaretLeftSquare />
+                    </LeftBtn>
+                    <RightBtn onClick={rightScroll}>
+                        <BsCaretRightSquare />
+                    </RightBtn>
                 </div>
                 <div className="relative w-full flex gap-6 snap-x snap-mandatory overflow-x-auto pb-4 " ref={scrollRef}>
 
-                    {
+                    {isLoading ? Array(10).fill().map(item => { return <FoodCard key={item} image /> }) :
 
-                        // food.map((foodItem) => {
-                        //     return <FoodCard foodItem={foodItem} key={foodItem._id} />;
+                        food?.data?.food.map((foodItem) => {
+                            return <FoodCard foodItem={foodItem} key={foodItem._id} />;
 
 
-                        // })
+                        })
 
 
 
                     }
 
                 </div>
-            </section>
+            </FoodSection>
         </>
     )
 }
 
 export default Food
+
+const FoodSection = tw.section`px-3 bg-white xl:px-20 pt-2 xl:pt-5 pb-10`
+
+const Head = tw.h1`text-orangefood headings text-center`
+const Button = tw.button`hidden mt-20 md:block backdrop-blur  absolute  z-20  text-white  px-4 py-3 text-3xl bg-black rounded  cursor-pointer hover:bg-grayfood`
+
+const LeftBtn = tw(Button)`
+left-0`
+
+const RightBtn = tw(Button)`
+right-0`
