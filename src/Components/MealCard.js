516a7c30ce1a4c90a29802ddcc31dd77c4536b12
ms29ash <ms29ash@gmail.com>
2022-05-18 22:56:30 +0530

    added loading in meals

--- src/Components/MealCard.js
+++ src/Components/MealCard.js
@@ -1,35 +1,41 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 import tw from "tailwind-styled-components";
 
-
 function MealCard(props) {
-    const { title, description, image } = props
+  const { title, description, image } = props;
 
-    return (
-        <>
-
-
-            <Wrapper>
-                <Link to="/food">
-                    <Img src={`images/${image}`} alt="" />
-                    <Text>
-                        <Head className="">{title}
-                        </Head>
-                        <Desc >{description}</Desc>
-                    </Text>
-                </Link>
-            </Wrapper>
-
-        </>
-    )
+  return (
+    <>
+      <Wrapper>
+        {image ? (
+          <Link to="/food">
+            <Img src={`images/${image}`} alt="" />
+            <Text>
+              <Head className="">{title}</Head>
+              <Desc>{description}</Desc>
+            </Text>
+          </Link>
+        ) : (
+          <>
+            <LoadImg></LoadImg>
+            <Text>
+              <Head className="">{title}</Head>
+              <Desc>{description}</Desc>
+            </Text>
+          </>
+        )}
+      </Wrapper>
+    </>
+  );
 }
 
-export default MealCard
+export default MealCard;
 
-const Wrapper = tw.div`card-container   mt-4 lg:mt-10 xl:mt-12   bg-white border-black-100 shadow-lg border-[1px] hover:shadow-2xl rounded-md`
+const Wrapper = tw.div`card-container   mt-4 lg:mt-10 xl:mt-12   bg-white border-black-100 shadow-lg border-[1px] hover:shadow-2xl rounded-md w-[90%]`;
 
-const Img = tw.img`object-cover rounded-t-md w-full h-[10rem]`
-const Text = tw.div`p-4`
-const Head = tw.h1`text-2xl font-bold meal-heading`
-const Desc = tw.p``
+const Img = tw.img`object-cover rounded-t-md w-full h-[10rem]`;
+const LoadImg = tw.div`object-cover rounded-t-md w-full h-[10rem] bg-gray-400`;
+const Text = tw.div`p-4`;
+const Head = tw.h1`text-2xl font-bold meal-heading`;
+const Desc = tw.p``;
--- src/Components/Meals.js
+++ src/Components/Meals.js
@@ -14,17 +14,21 @@ function TypeMeals() {
   const fetchMeals = async () => {
     return axios.get('/food/meals')
   }
-  const { data: meals, isLoading, isError, error } = useQuery('meals', fetchMeals)
+  const { isLoading, data: meals, isError, error } = useQuery('meals', fetchMeals)
 
 
 
   return (
     <>
-      <Meals Mealsid="services">
+      <Meals id="services">
         <MealHeading>Meals</MealHeading>
-        {meals?.data?.meals.map((meal) => {
-          return <MealCard title={meal.title} description={meal.description} image={meal.image} key={meal._id} />
-        })
+        {
+
+          isError || isLoading ? Array(3).fill().map(item => { return <MealCard key={item} /> }) :
+
+            meals?.data?.meals.map((meal) => {
+              return <MealCard title={meal.title} description={meal.description} image={meal.image} key={meal._id} />
+            })
         }
       </Meals>
 
@@ -34,5 +38,5 @@ function TypeMeals() {
 
 export default TypeMeals
 
-const Meals = tw.section`bg-white grid grid-cols-1  md:grid-cols-3 gap-x-10 justify-items-center sm:px-12 px-8 lg:px-20 pt-2 xl:pt-5 pb-16`
+const Meals = tw.section`bg-white grid grid-cols-1 w-screen md:grid-cols-3 gap-x-10 justify-items-center sm:px-12 px-8 lg:px-20 pt-2 xl:pt-5 pb-16`
 const MealHeading = tw.h1`text-orangefood headings md:col-span-3`
