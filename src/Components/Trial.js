944c5d9bafe06e7a1325682ee3b87d7ab04014b4
ms29ash <ms29ash@gmail.com>
2022-05-21 08:48:23 +0530

    added tw and fix position and btn

--- src/Components/Trial.js
+++ src/Components/Trial.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 import '../Styles/Trial.css'
+import tw from "tailwind-styled-components";
+
 
 function Trial() {
     return (
-        <div className="pt-[15vh] md:h-[80vh] h-[50vh] bg-blend-overlay " id="trial">
-            <div className="flex flex-col">
-                <p className="lg:text-5xl md:text-4xl text-2xl text-center my-2 lg:my-5 font-bold text-white">Get a Trial</p>
-                <p className="lg:text-5xl md:text-4xl text-2xl text-center my-2 lg:my-5 font-bold text-orangefood">Make Your Life Easy</p>
-                <Link to="/trial" className=" text-center md:text-2xl  hover:bg-orangefood bg-redfood rounded-3xl px-3 py-2 lg:px-5  text-white text-xl lg:text-2xl mt-5 mx-auto"> Get a free trial</Link>
-            </div>
-        </div>
+        <Container id="trial">
+            <Wrapper >
+                <TextTop className="">Get a Trial</TextTop>
+                <TextBottom >Make Your Life Easy</TextBottom>
+                <Button to="/trial" > Get a free trial</Button>
+            </Wrapper>
+        </Container>
     )
 }
 
 export default Trial
+
+const Container = tw.div`grid place-items-center  md:h-[80vh] h-[50vh] bg-blend-overlay`
+
+const Wrapper = tw.div`flex flex-col`
+
+const Button = tw(Link)` text-center md:text-2xl  hover:bg-orangefood bg-redfood rounded-3xl px-24 py-4   text-white text-xl lg:text-2xl mt-5 mx-auto`
+
+const Text = tw.p`lg:text-5xl md:text-4xl text-2xl text-center my-2 lg:my-5 font-bold `
+
+const TextTop = tw(Text)`text-white`
+const TextBottom = tw(Text)`text-orangefood`
