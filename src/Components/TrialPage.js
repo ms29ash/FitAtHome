8875e6482c2eee367d4be1fd6fed3117ed66e652
ms29ash <ms29ash@gmail.com>
2022-06-02 06:50:32 +0530

    added button and details in trialpage

--- src/Components/TrialPage.js
+++ src/Components/TrialPage.js
@@ -7,11 +7,70 @@ function TrialPage() {
             <Wrapper>
                 <Img src='./images/trailpageimg.jpg' />
                 <Card>
-                    <TopHead>Free Trial</TopHead>
-                    <Head>What you Get</Head>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta deserunt rerum sint debitis hic nisi assumenda autem officiis! Tempore aspernatur vel esse praesentium sequi enim. Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit distinctio consequatur beatae impedit eius asperiores voluptate natus eaque aperiam. Et dolorum vel illum suscipit quo qui, dolores optio ut earum numquam, inventore, non voluptatum impedit. Similique laboriosam inventore sint nostrum, maiores asperiores ducimus quam a.</p>
-                    <Button>Start Trial</Button>
-                    <SmallText>free for one month</SmallText>
+                    <Trial>
+
+                        <TopHead>Free Trial</TopHead>
+
+                        <Button>Start Trial</Button>
+
+                    </Trial>
+                    <Details>
+
+                        <Head>What you Get1</Head>
+                        <ul>
+                            <li>Custom tailored diet plans.
+
+                            </li>
+                            <li>Free from refined sugars & trans fats.
+                            </li>
+                            <li>Different menu every day.</li>
+                            <li>
+                                Flexibility of plans
+                            </li>
+                            <li>Ongoing nutritionist support.</li>
+                        </ul>
+                        <Head>What you Get1</Head>
+                        <ul>
+                            <li>Custom tailored diet plans.
+
+                            </li>
+                            <li>Free from refined sugars & trans fats.
+                            </li>
+                            <li>Different menu every day.</li>
+                            <li>
+                                Flexibility of plans
+                            </li>
+                            <li>Ongoing nutritionist support.</li>
+                        </ul>
+                        <Head>What you Get1</Head>
+                        <ul>
+                            <li>Custom tailored diet plans.
+
+                            </li>
+                            <li>Free from refined sugars & trans fats.
+                            </li>
+                            <li>Different menu every day.</li>
+                            <li>
+                                Flexibility of plans
+                            </li>
+                            <li>Ongoing nutritionist support.</li>
+                        </ul>
+                        <Head>What you Get1</Head>
+                        <ul>
+                            <li>Custom tailored diet plans.
+
+                            </li>
+                            <li>Free from refined sugars & trans fats.
+                            </li>
+                            <li>Different menu every day.</li>
+                            <li>
+                                Flexibility of plans
+                            </li>
+                            <li>Ongoing nutritionist support.</li>
+                        </ul>
+
+                    </Details>
+
                 </Card>
             </Wrapper>
         </Container>
@@ -19,11 +78,14 @@ function TrialPage() {
 }
 
 export default TrialPage
-const Container = tw.div` h-screen w-screen grid place-items-center	`
-const Wrapper = tw.div`flex items-center mx-auto w-[70vw] h-[60vh] bg-white shadow-xl overflow-hidden`
-const Img = tw.img` w-[50%] h-full  object-cover`
-const Card = tw.div`p-20`
-const TopHead = tw.h1`text-4xl my-5 `
+const Container = tw.div` mt-[15vh] 	`
+const Wrapper = tw.div` flex items-center mx-auto w-[90vw]  shadow-xl overflow-hidden flex-col`
+const Img = tw.img` w-full max-h-[50vh]  object-cover`
+const Card = tw.div`-mt-[5vh] w-full rounded-xl bg-white `
+
+const Trial = tw.div`flex justify-between	 mb-5 shadow-redfood shadow-lg py-4 px-3 w-full items-center	`
+const TopHead = tw.h1`text-3xl  `
+const Button = tw.button` bg-redfood hover:bg-orangefood  text-white  py-4 rounded-full  w-[30%] min-w-[100px] max-w-[200px] font-bold text-sm `
+
+const Details = tw.div`overflow-scroll px-5   max-h-[70vh]`
 const Head = tw.h3`text-2xl `
-const Button = tw.button` bg-orangefood text-white px-8 py-2 rounded-lg mt-6 hover:bg-orangefood`
-const SmallText = tw.p` block  ml-1`
