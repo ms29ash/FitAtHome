20cf2f0562af1a0a5f00cd41c46e2b62a670d65e
ms29ash <ms29ash@gmail.com>
2022-06-02 11:54:19 +0530

    added tw and create signin and signup page

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useContext } from 'react'
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
-import '../Styles/SignIn.css'
+import tw from 'tailwind-styled-components'
 
 
 
@@ -22,36 +22,46 @@ function SignIn(props) {
 
 
     return (
-        <div className="signin-page py-24 w-screen h-screen mt-[10vh]">
-            <div className=" bg-white md:w-[50%] w-[70%] shadow-xl rounded-lg  mx-auto px-8 py-12 ">
-                <h1 className="text-center text-orangefood">Welcome</h1>
+        <Container className="signin-page ">
+            <Wrapper >
+                <Head>Welcome</Head>
 
-                <form className="bg-white " onSubmit={handleSubmit(onSubmit)} >
-                    <label htmlFor="email" className="ml-3 text-greenfood">Email</label>
-                    <input className="w-full py-2 px-3  border-b-2 !outline-none border-greenfood" placeholder="Email" type="email"  {...register("email", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i })} />
-                    <div className="h-5 text-redfood text-xs ml-2">
+                <Form onSubmit={handleSubmit(onSubmit)} >
+                    <Label htmlFor="email" >Email</Label>
+                    <Input placeholder="Email" type="email"  {...register("email", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i })} />
+                    <Error >
 
                         {errors.email?.type === 'required' ? "Email is required" : errors.email?.type === 'pattern' ? "Invalid email" : ""}
-                    </div>
+                    </Error>
                     <br />
 
 
-                    <label htmlFor="Password" className="ml-3 text-greenfood">Password</label>
-                    <input className="w-full py-2 px-3  border-b-2 !outline-none border-greenfood" type="password" placeholder="Password"   {...register("password", { required: true, minLength: 5 })} />
-                    <div className="h-5 text-redfood text-xs ml-2">
+                    <Label htmlFor="Password" >Password</Label>
+                    <Input className="w-full py-2 px-3  border-b-2 !outline-none border-greenfood" type="password" placeholder="Password"   {...register("password", { required: true, minLength: 5 })} />
+                    <Error>
 
                         {errors.password?.type === 'required' ? "Password is required" : errors.password?.type === 'minLength' ? "Too Short" : ""}
-                    </div>
-                    <p className="mt-5 text-sm text-center"> <Link className="hover:text-orangefood text-grayfood" to='/forgetpass'>Forget Password</Link>  </p>
+                    </Error>
+                    <Text> <Link className="hover:text-orangefood text-grayfood" to='/forgetpass'>Forget Password</Link>  </Text>
                     <br />
 
-                    <button className=" !bg-redfood text-center w-full px-5 py-2 text-white rounded-lg hover:!bg-orangefood cursor-pointer" type="submit" >Sign In</button>
-                </form>
-                <p className="mt-5 text-sm text-center">Create an account <Link className="hover:text-orangefood underline" to='/signup'>SignUp</Link>  </p>
+                    <Button type="submit" >Sign In</Button>
+                </Form>
+                <Text>Create an account <Link className="hover:text-orangefood underline" to='/signup'>SignUp</Link>  </Text>
 
-            </div >
-        </div>
+            </ Wrapper>
+        </Container>
     )
 }
 
 export default SignIn
+
+const Container = tw.div`py-24 w-screen h-screen mt-[10vh]`
+const Wrapper = tw.div`bg-white w-[95vh] max-w-[600px] shadow-xl rounded-lg  mx-auto px-8 py-12 `
+const Head = tw.h1`text-center text-orangefood`
+const Form = tw.form`bg-white`
+const Label = tw.label`ml-3 text-greenfood`
+const Input = tw.input`w-full py-2 px-3  border-b-2 !outline-none border-greenfood`
+const Error = tw.div`h-5 text-redfood text-xs ml-2`
+const Text = tw.p`mt-5 text-sm text-center`
+const Button = tw.button`!bg-redfood text-center w-full px-5 py-2 text-white rounded-full hover:!bg-orangefood cursor-pointer`
--- src/Components/TrialPage.js
+++ src/Components/TrialPage.js
@@ -9,71 +9,79 @@ function TrialPage() {
                 <Card>
                     <Trial>
 
-                        <TopHead>Free Trial</TopHead>
+                        <TopHead>Get One Week Free</TopHead>
 
                         <Button>Start Trial</Button>
 
                     </Trial>
                     <Details>
 
-                        <Head>What you Get1</Head>
-                        <ul>
-                            <li>Custom tailored diet plans.
+                        <Group>
 
-                            </li>
-                            <li>Free from refined sugars & trans fats.
-                            </li>
-                            <li>Different menu every day.</li>
-                            <li>
-                                Flexibility of plans
-                            </li>
-                            <li>Ongoing nutritionist support.</li>
-                        </ul>
-                        <Head>What you Get1</Head>
-                        <ul>
-                            <li>Custom tailored diet plans.
+                            <Head>What you Get1</Head>
+                            <ul>
+                                <Li>Custom tailored diet plans.
 
-                            </li>
-                            <li>Free from refined sugars & trans fats.
-                            </li>
-                            <li>Different menu every day.</li>
-                            <li>
-                                Flexibility of plans
-                            </li>
-                            <li>Ongoing nutritionist support.</li>
-                        </ul>
-                        <Head>What you Get1</Head>
-                        <ul>
-                            <li>Custom tailored diet plans.
+                                </Li>
+                                <Li>Free from refined sugars & trans fats.
+                                </Li>
+                                <Li>Different menu every day.</Li>
+                                <Li>
+                                    Flexibility of plans
+                                </Li>
+                                <Li>Ongoing nutritionist support.</Li>
+                            </ul>
+                        </Group>
+                        <Group>
+                            <Head>What you Get1</Head>
+                            <ul>
+                                <Li>Custom tailored diet plans.
 
-                            </li>
-                            <li>Free from refined sugars & trans fats.
-                            </li>
-                            <li>Different menu every day.</li>
-                            <li>
-                                Flexibility of plans
-                            </li>
-                            <li>Ongoing nutritionist support.</li>
-                        </ul>
-                        <Head>What you Get1</Head>
-                        <ul>
-                            <li>Custom tailored diet plans.
+                                </Li>
+                                <Li>Free from refined sugars & trans fats.
+                                </Li>
+                                <Li>Different menu every day.</Li>
+                                <Li>
+                                    Flexibility of plans
+                                </Li>
+                                <Li>Ongoing nutritionist support.</Li>
+                            </ul>
+                        </Group>
+                        <Group>
+                            <Head>What you Get1</Head>
+                            <ul>
+                                <Li>Custom tailored diet plans.
 
-                            </li>
-                            <li>Free from refined sugars & trans fats.
-                            </li>
-                            <li>Different menu every day.</li>
-                            <li>
-                                Flexibility of plans
-                            </li>
-                            <li>Ongoing nutritionist support.</li>
-                        </ul>
+                                </Li>
+                                <Li>Free from refined sugars & trans fats.
+                                </Li>
+                                <Li>Different menu every day.</Li>
+                                <Li>
+                                    Flexibility of plans
+                                </Li>
+                                <Li>Ongoing nutritionist support.</Li>
+                            </ul>
+                        </Group>
+                        <Group>
+                            <Head>What you Get1</Head>
+                            <ul>
+                                <Li>Custom tailored diet plans.
 
+                                </Li>
+                                <Li>Free from refined sugars & trans fats.
+                                </Li>
+                                <Li>Different menu every day.</Li>
+                                <Li>
+                                    Flexibility of plans
+                                </Li>
+                                <Li>Ongoing nutritionist support.</Li>
+                            </ul>
+                        </Group>
                     </Details>
 
                 </Card>
             </Wrapper>
-        </Container>
+        </Container >
     )
 }
 
@@ -83,9 +91,11 @@ const Wrapper = tw.div` flex items-center mx-auto w-[90vw]  shadow-xl overflow-h
 const Img = tw.img` w-full max-h-[50vh]  object-cover`
 const Card = tw.div`-mt-[5vh] w-full rounded-xl bg-white `
 
-const Trial = tw.div`flex justify-between	 mb-5 shadow-redfood shadow-lg py-4 px-3 w-full items-center	`
-const TopHead = tw.h1`text-3xl  `
+const Trial = tw.div`flex justify-between	 mb-5 shadow-redfood shadow-md py-4 px-[5vw] w-full items-center	`
+const TopHead = tw.h1`text-2xl  `
 const Button = tw.button` bg-redfood hover:bg-orangefood  text-white  py-4 rounded-full  w-[30%] min-w-[100px] max-w-[200px] font-bold text-sm `
 
-const Details = tw.div`overflow-scroll px-5   max-h-[70vh]`
-const Head = tw.h3`text-2xl `
+const Details = tw.div`overflow-scroll px-[5vw]   max-h-[70vh]`
+const Group = tw.div`my-5`
+const Head = tw.h3`text-xl `
+const Li = tw.li`list-disc	`
