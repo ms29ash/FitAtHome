62613fde4493bff021df0c7b1c01c013da8ac199
ms29ash <ms29ash@gmail.com>
2023-01-14 12:40:17 +0530

    Fix footer text size

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -85,8 +85,8 @@ export default Footer;
 
 const Container = tw.footer`md:pt-12 pt-2 pb-6 bg-redfood grid  md:grid-cols-5    items-start  text-white grid-cols-2`;
 const Wrapper = tw.div`flex flex-col mt-6 md:mt-4 ml-[20%]`
-const Head = tw.h3`text-xl mb-3`
+const Head = tw.h3`text-lg mb-3`
 const Img = tw.img`w-[30%] md:w-[50%] mx-auto`
-const LinkTo = tw(Link)`text-gray-300 hover:text-white my-3 text-md  `;
+const LinkTo = tw(Link)`text-gray-300 hover:text-white my-2 text-sm  `;
 const Social = tw.div` flex w-screen justify-center flex-col items-center py-6 text-redfood`
 const Icons = tw.div`flex text-3xl space-x-7 mt-2 `
