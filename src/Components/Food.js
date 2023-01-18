0f563074c54a75b0b970b7a27173009eb62f6582
ms29ash <ms29ash@gmail.com>
2023-01-18 16:24:00 +0530

    Fix Food Card size

--- src/Components/Food.js
+++ src/Components/Food.js
@@ -31,7 +31,7 @@ function Food() {
     return (
         <>
             <FoodSection id="food">
-                <h1 className="w-full text-center text-redfood xl:text-4xl mb-4 text-3xl  xl:mb-6" >Best Healthy Food</h1>
+                <h1 className="w-full  text-redfood xl:text-3xl mb-4 text-2xl mb-4  xl:mb-8" >Best Healthy Food</h1>
 
                 <div className="flex justify-between w-screen z-20">
                     <LeftBtn onClick={leftScroll} className="btn-arrow">
@@ -43,7 +43,7 @@ function Food() {
 
                     </RightBtn>
                 </div>
-                <div className="relative w-full flex gap-6 snap-x snap-mandatory overflow-x-auto pb-4 " id="food-section" ref={scrollRef}>
+                <div className="relative w-full flex items-stretch gap-6 snap-x snap-mandatory overflow-x-auto pb-16 " id="food-section" ref={scrollRef}>
 
                     {isError || isLoading ? Array(10).fill().map((item, index) => { return <FoodCard key={index} /> }) :
 
@@ -65,7 +65,7 @@ function Food() {
 
 export default Food
 
-const FoodSection = tw.section`px-3 bg-white my-6 xl:px-20 pt-2 xl:pt-5 pb-2`
+const FoodSection = tw.section`px-3 bg-white  xl:px-20  pt-16 xl:pt-6 pb-10 py-16`
 const Button = tw.button`hidden mt-20 md:block w-16  h-20 absolute  z-20  text-black  px-4 py-3 text-5xl bg-white rounded stroke-1  cursor-pointer`
 
 const LeftBtn = tw(Button)`
--- src/Components/FoodCard.js
+++ src/Components/FoodCard.js
@@ -2,27 +2,24 @@ import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import tw from "tailwind-styled-components";
 import { Link } from "react-router-dom";
+import { FaStar } from "react-icons/fa";
 
 function FoodCard(props) {
   let { foodItem } = props;
-  const description = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente vitae quam yess recusandae.'
 
   return (
     <>
       <Container>
         <Wrapper>
-          <Link to={`food/foodDetail/${foodItem?._id}`} >
+          <Link to={`food/foodDetail/${foodItem?._id}`}>
             {
               <FoodImg
-
                 src={foodItem?.image}
                 alt=""
                 placeholder={<LoadFoodImg />}
                 threshold={100}
               />
             }
-
-
 
             <FoodTypeIcon
               src={
@@ -37,11 +34,26 @@ function FoodCard(props) {
               alt=""
             />
 
-
             <Text>
-              <TextHead className="">{foodItem?.name}</TextHead>
-              <p className="mb-3 text-md h-[50%] ">{description}</p>
-              <p className=" mb-3 h-[15%]  text-redfood text-2xl font-bold ">&#8377; {foodItem?.price}</p>
+              {
+                foodItem?.name.length < 30 ?
+
+                  <TextHead className="mb-2">{foodItem?.name.slice(0, 30)}</TextHead>
+                  :
+                  <TextHead className="mb-2">{foodItem?.name.slice(0, 27)}..</TextHead>
+              }
+              <Details>
+
+                <p className="  h-[15%]  leading-8 text-black-800 text-lg  ">
+                  &#8377; {foodItem?.price}
+                </p>
+                <ReviewBox >
+
+                  <FaStar style={{ color: 'white' }} />
+                  <Review >{foodItem?.ratings}</Review>
+                </ReviewBox>
+              </Details>
+
             </Text>
           </Link>
         </Wrapper>
@@ -52,12 +64,15 @@ function FoodCard(props) {
 
 export default FoodCard;
 
-const Container = tw.div`scroll-end scroll-mx-5 shrink-0`;
-const Wrapper = tw.div`card-container my-3  rounded-md w-[250px] aspect-[3/5] hover:bg-redfood/20  transition-all pt-2 bg-gray-100`;
+const Container = tw.div`scroll-end scroll-mx-5 shrink-0   `;
+const Wrapper = tw.div`card-container my-3 transition-all   rounded-md w-[250px]  h-full  hover:shadow-2xl duration-300  transition-all bg-white shadow-lg`;
 const FoodImg = tw(
   LazyLoadImage
-)`rounded-t-md object-cover w-[95%]  mx-auto aspect-[4/3] bg-white `;
+)`rounded-t-md object-cover w-[100%]  mx-auto aspect-[4/3] bg-white `;
 const LoadFoodImg = tw.img`rounded-t-md object-cover w-[250px] h-[187px] bg-gray-400 `;
-const FoodTypeIcon = tw.img`absolute top-8 ml-3 w-[30px] h-[30px]`;
-const Text = tw.div`p-[8px] h-[55%]`;
-const TextHead = tw.h1`text-xl h-[30%] font-bold text-redfood`;
+const FoodTypeIcon = tw.img`absolute top-8 ml-3 w-[25px] h-[25px]`;
+const Text = tw.div`p-[8px] `;
+const Details = tw.div`flex justify-between items-center w-full`
+const TextHead = tw.h1`text-sm h-[30%] font-bold text-black`;
+const ReviewBox = tw.div`flex justify-between items-center rounded-md bg-grayfood px-2 `
+const Review = tw.div`rounded-sm bg-grayfood text-white ml-1`
