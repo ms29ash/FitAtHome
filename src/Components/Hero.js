2597c266a709c11bf8ce715147c3908404d984ca
ms29ash <ms29ash@gmail.com>
2023-01-28 09:06:37 +0530

    Change button Styles in Hero

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
 import tw from "tailwind-styled-components";
 import { Link } from "react-router-dom"
 
@@ -19,9 +16,8 @@ function Hero() {
                     <Buttons>
 
                         <Button to="/food" >Order Now</Button>
-                        <Button to="/trial" >Free Trial</Button>
+                        <TrialButton to="/trial" >Free Trial</TrialButton>
                     </Buttons>
-                    {/* <Img src="./images/intro.jpg" alt="" /> */}
 
                 </Container>
 
@@ -29,15 +25,17 @@ function Hero() {
             </Wrapper>
 
 
-            {/* </Carousel> */}
         </>
     )
 }
 
 export default Hero
+
+
 const Wrapper = tw.div`
 w-screen  overflow-hidden `
 const Container = tw.div` w-screen h-screen max-h-[600px] intro flex flex-col justify-center items-start px-[10vw]`
 const Buttons = tw.div` flex items-center my-5`
-const Button = tw(Link)` bg-redfood hover:bg-orangefood cursor-pointer text-white mr-4 px-5 py-3 rounded-full`
-const Head = tw.h1`md:text-6xl text-[2.65rem] leading-[3.75rem] capitalize w-full md:w-[80%] lg:w-[60%] lg:leading-[4.25rem]`
+const Button = tw(Link)` bg-redfood hover:bg-orangefood cursor-pointer text-white mr-4 px-5 py-3 rounded-full font-bold`
+const TrialButton = tw(Link)` cursor-pointer text-redfood mr-4 px-5 py-3 rounded-full  border-2 border-redfood  hover:border-orangefood hover:text-white hover:bg-redfood font-bold `
+const Head = tw.h1`md:text-6xl  text-[2.65rem] leading-[3.75rem] capitalize w-full md:w-[80%] lg:w-[60%] lg:leading-[4.25rem]`
