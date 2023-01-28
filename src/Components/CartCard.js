701e2adfe1791ca701f3cd8c604ba7981216bd50
ms29ash <ms29ash@gmail.com>
2023-01-28 22:48:30 +0530

    Add remove button and details in CartCard

--- src/Components/CartCard.js
+++ src/Components/CartCard.js
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux'
 import { setQuantity, removeCart } from '../features/basket/basketSlice'
 
 function CartCard({ item }) {
-    const { image, name, price } = item.item;
+    const { image, name, price, type } = item.item;
     const dispatch = useDispatch();
     const refSelect = useRef()
 
@@ -17,33 +17,65 @@ function CartCard({ item }) {
     }
 
     return (
-        <Container>
-            <Img src={image} alt="" />
-            <Wrapper>
+        <>
+            <hr />
+            <Container>
+                <Img src={image} alt="" />
+                <Wrapper>
 
-                <p className="text-lg font-bold">{name}</p>
-                <p className=" mb-3 h-[15%]  text-redfood text-xl font-bold ">&#8377; {price}</p>
-                <Select name="quantity" ref={refSelect} value={item.quantity} onChange={onChangeHandler} >
-                    <option value="0">0</option>
-                    {
-                        Array(10).fill().map((item, index) => <option value={index + 1} key={index}  >{index + 1}</option>)
-                    }
+                    <p className="text-lg mt-[10%] font-bold">{name}</p>
+                    <FoodTypeIcon>
+                        <img
+                            className='w-[25px] h-[25px] mr-2'
+                            src={
+                                type === "Veg"
+                                    ? "images/veg_icon.png"
+                                    : type === "Non-Veg"
+                                        ? "images/nonveg_icon.png"
+                                        : type === "Vegan"
+                                            ? "images/vegan_icon.png"
+                                            : ""
+                            }
+                            alt=""
+                        />
+                        {type}
+                    </FoodTypeIcon>
+                    <Price className="  ">&#8377; {price}</Price>
 
-                </Select>
-            </Wrapper>
+                    <Select name="quantity" ref={refSelect} value={item.quantity} onChange={onChangeHandler} >
+                        <option value="0">0</option>
+                        {
+                            Array(10).fill().map((item, index) => <option value={index + 1} key={index}  >{index + 1}</option>)
+                        }
 
-        </Container>
+                    </Select>
+                    <div className="flex mt-4 text-grayfood  mb-4" >
+
+                        <Btn>Remove</Btn>
+
+                        <Btn  >Add to Favorites</Btn>
+
+                    </div>
+                </Wrapper>
+
+            </Container>
+        </>
     )
 }
 
 export default CartCard
 
 const Container = tw.div`
-w-full h-26 my-5 flex 
+w-full h-26 my-3 flex relative  
 `
 const Img = tw.img`
-w-[30%] object-cover aspect-square md:aspect-[4/3]
+w-[30%] max-w-[250px] object-cover aspect-square md:aspect-[4/3]
 `
 const Wrapper = tw.div` ml-4`
 
-const Select = tw.select` drop-shadow-4xl bg-gray-200 p-1 rounded-lg mt-2`
+const Select = tw.select` drop-shadow-4xl shadow-lg bg-gray-200 px-2 py-1 text-base rounded-lg mt-2 border-[2px] border-slate-400 mt-[6%] mb-[4%] `
+const FoodTypeIcon = tw.div` my-2  items-center flex`;
+
+const Price = tw.p`my-3 h-[15%] absolute top-2 right-2  text-redfood text-xl font-bold`
+
+const Btn = tw.button` mr-4 hover:text-redfood text-sm hover:underline`
