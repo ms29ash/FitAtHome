ef7f15fcf023f99acd24e46307aeda456b743f15
ms29ash <ms29ash@gmail.com>
2023-02-28 22:08:46 +0530

    change button and Text of Hero

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -12,11 +12,12 @@ function Hero() {
 
             <Wrapper>
                 <Container>
-                    <Head>Eat less from box & more from the Earth</Head>
+                    <Head>Healthy food Home Delivery</Head>
+                    <SubText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aspernatur veniam iure totam, libero modi voluptatem omnis</SubText>
                     <Buttons>
 
-                        <Button to="/food" >Order Now</Button>
-                        <TrialButton to="/trial" >Free Trial</TrialButton>
+                        <Button to="/food" >Place an Order</Button>
+                        <TrialButton to="/trial" >Get a Free Trial</TrialButton>
                     </Buttons>
 
                 </Container>
@@ -36,6 +37,7 @@ const Wrapper = tw.div`
 w-screen  overflow-hidden `
 const Container = tw.div` w-screen h-screen max-h-[600px] intro flex flex-col justify-center items-start px-[10vw]`
 const Buttons = tw.div` flex items-center my-5`
-const Button = tw(Link)` bg-redfood hover:bg-orangefood cursor-pointer text-white mr-4 px-5 py-3 rounded-full font-bold`
-const TrialButton = tw(Link)` cursor-pointer text-redfood mr-4 px-5 py-3 rounded-full  border-2 border-redfood  hover:border-orangefood hover:text-white hover:bg-redfood font-bold `
-const Head = tw.h1`md:text-6xl  text-[2.65rem] leading-[3.75rem] capitalize w-full md:w-[80%] lg:w-[60%] lg:leading-[4.25rem]`
+const Button = tw(Link)` bg-redfood hover:bg-orangefood cursor-pointer text-white mr-4 px-5 py-3 rounded-full font-bold shadow-md hover:shadow-xl`
+const TrialButton = tw(Link)` cursor-pointer text-black mr-4 px-5 py-3 rounded-full   bg-white  font-bold shadow-md hover:shadow-2xl`
+const Head = tw.h1`md:text-4xl  text-[2.65rem] leading-[3.75rem] capitalize w-full md:w-[80%] lg:w-[50%] lg:leading-[4.25rem]`
+const SubText = tw.p`lg:w-[50%] md:w-[80%] w-full text-slate-600  `
