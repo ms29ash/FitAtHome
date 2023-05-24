4d321305bb42a70ded2b95544c29528f2bd8b446
ms29ash <ms29ash@gmail.com>
2023-05-24 14:18:04 +0530

    add isLoggedIn in dependency

--- src/Components/Routes/AuthRoute.js
+++ src/Components/Routes/AuthRoute.js
@@ -24,7 +24,7 @@ function AuthRoute({ children }) {
             }
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    }, [isLoggedIn])
 
     return (
         <>
