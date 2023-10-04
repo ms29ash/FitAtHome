8d00e2e229f2217ba2c7cd40cfb585e083b904ae
ms29ash <ms29ash@gmail.com>
2023-10-04 19:07:32 +0530

    New Category Section

--- src/Components/Home/Category.js
+++ src/Components/Home/Category.js
@@ -26,7 +26,7 @@ function TypeMeals() {
 
             isSuccess ?
               data?.data.categories?.map((category) => {
-                return <CategoryCard title={category.title} description={category.description} image={category.image} key={category._id} />
+                return <CategoryCard title={category.title} description={category.description} image={category.image} key={category._id} icon={category.icon} />
               })
               :
               Array(4).fill().map((item, index) => { return <CategoryLoading key={index} /> })
@@ -40,6 +40,6 @@ function TypeMeals() {
 
 export default TypeMeals
 
-const Category = tw.section`bg-white  w-screen items-center flex flex-col  justify-center px-6 md:pb-20 pt-16`
-const Heading = tw.h1`text-redfood text-lg headings md:col-span-4`
-const Wrapper = tw.div`flex w-fit flex-wrap justify-between`
+const Category = tw.section`bg-white  w-screen  items-center flex flex-col  justify-center px-6 md:pb-20 pt-16`
+const Heading = tw.h1`text-black text-lg headings md:col-span-4`
+const Wrapper = tw.div`  max-w-[1400px] grid grid-cols-4 w-fit flex-wrap justify-between gap-x-5 `
--- src/Components/Home/CategoryCard.js
+++ src/Components/Home/CategoryCard.js
@@ -4,10 +4,12 @@ import tw from "tailwind-styled-components";
 import { useDispatch } from "react-redux";
 import { setType } from "../../features/filter/foodFilterSlice";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import { AiFillRightCircle } from "react-icons/ai"
+import { ClassNames } from "@emotion/react";
 
 function CategoryCard(props) {
   const dispatch = useDispatch();
-  const { title, image } = props;
+  const { title, image, icon } = props;
 
   return (
     <>
@@ -21,13 +23,24 @@ function CategoryCard(props) {
             <Img
               placeholder={<LoadImg />}
               threshold={100}
-              // src={`images/${image}`}
-              src={image}
+              src={`images/category/${image}`}
+              // src={image}
               alt=""
             />
-            <Text>
-              <Head className="flex-col">{title}</Head>
-            </Text>
+            <Detail>
+              <Text>
+
+                <Image
+                  placeholder={<LoadImg />}
+                  threshold={100}
+                  src={`images/category/${icon}`}
+                  // src={image}
+                  alt=""
+                />
+                <Head >{title}</Head>
+              </Text>
+              <  AiFillRightCircle className=" text-2xl text-ssorange" />
+            </Detail>
           </Link>
         }
       </Wrapper>
@@ -37,9 +50,11 @@ function CategoryCard(props) {
 
 export default CategoryCard;
 
-const Wrapper = tw.div`card-container transition-all   mt-4 lg:mt-10 xl:mt-12  bg-white duration-300 shadow-lg border-[1px] hover:shadow-2xl rounded sm:w-[23%] max-w-[250px] hover:transform:scale(1.05) w-[48%] mx-[1%]`;
+const Wrapper = tw.div`card-container transition-all   mt-4 lg:mt-10 xl:mt-12  bg-white duration-300 shadow-lg border-[1px] rounded-md  aspect-[5/4] relative object-cover before:bg-gradient-to-t before:from-black  before:via-black/10 before:to-black/0 before:absolute before:top-0 before:bottom-0 before:left-0 before:right-0   `;
 
-const Img = tw(LazyLoadImage)`object-contain rounded-t-md w-full aspect-[5/2.75]`;
+const Img = tw(LazyLoadImage)`object-fill rounded-md h-full w-full `;
 const LoadImg = tw.div`object-cover rounded-t-md w-full h-[10rem] bg-gray-400`;
-const Text = tw.div`p-4`;
-const Head = tw.h1`text-center font-bold text-[100%] meal-heading`;
+const Detail = tw.div`px-4 py-2 rounded-md absolute flex items-center bg-white bottom-4 right-2 left-2 justify-between  `;
+const Text = tw.div` flex items-center `;
+const Head = tw.h3`text-center font-bold  text-base ml-2 `;
+const Image = tw.img` w-6 `
