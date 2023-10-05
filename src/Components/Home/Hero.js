a7b8e10f7e89858cccfa80b20800de905dd0cf33
ms29ash <ms29ash@gmail.com>
2023-10-05 07:31:02 +0530

    Add Responsivenes in hero section

--- src/Components/Home/Hero.js
+++ src/Components/Home/Hero.js
@@ -12,10 +12,14 @@ function Hero() {
 
             <Wrapper>
                 <Container>
-                    <UpperText>Savor Health, Savor Life</UpperText>
-                    <Head>Where<br />Taste meets<br />wellness</Head>
-                    <SubText>Unlock Your Journey to a Fitter, Healthier<br />
-                        You with Sweet and Savory Nutritious Delights!</SubText>
+                    <div className="h-[20%] md:hidden invisible" > </div>
+                    <Text>
+
+                        <UpperText>Savor Health, Savor Life</UpperText>
+                        <Head>Where<br />Taste meets<br />wellness</Head>
+                        <SubText>Unlock Your Journey to a Fitter, Healthier
+                            You with Sweet and Savory Nutritious Delights!</SubText>
+                    </Text>
                     <Buttons>
 
                         <Button to="/food" >Learn More</Button>
@@ -36,11 +40,15 @@ export default Hero
 
 
 const Wrapper = tw.div`
-w-screen  overflow-hidden `
-const Container = tw.div` w-screen   intro aspect-[2/1] flex flex-col justify-center items-start px-[10vw]`
-const Buttons = tw.div` flex items-center my-5`
-const Button = tw(Link)` bg-ssorange hover:brightness-60 cursor-pointer text-white mr-4 px-10 py-4 rounded-xl font-bold hover:shadow-md `
-const TrialButton = tw(Button)` cursor-pointer mr-4    bg-ssgreen `
-const Head = tw.h1`text-[4.5rem] text-black  leading-[5rem] capitalize w-full md:w-[80%] lg:w-[50%] `
-const SubText = tw.p`lg:w-[50%] md:w-[80%] mt-3 w-full text-lg  text-black/80  `
+w-screen  overflow-hidden h-screen `
+const Container = tw.div` w-screen   intro aspect-[2/1] flex flex-col md:justify-center justify-around items-start px-[10vw] h-full `
+
+const Text = tw.div` w-full text-lg 3xl:text-[2xl] `
+const Head = tw.h1`text-[2.5rem] md:text-[3.5rem] xl:text-[4rem] 3xl:text-[8rem] leading-[3rem] md:leading-[4rem] xl:leading-[5rem] 3xl:leading-[9rem] capitalize w-full md:w-[80%] lg:w-[50%] `
+const SubText = tw.p`lg:w-[50%] md:w-[80%] mt-3 w-full   text-black/80  `
 const UpperText = tw.p`lg:w-[50%] md:w-[80%] w-full  text-ssorange  `
+
+
+const Buttons = tw.div` flex flex-col md:flex-row space-y-2 md:space-y-0 items-center my-5 w-full   `
+const Button = tw(Link)` bg-ssorange hover:brightness-60 cursor-pointer text-white mr-4 py-4 rounded-xl font-bold hover:shadow-md  text-center w-[100%] md:!w-[200px]   `
+const TrialButton = tw(Button)` cursor-pointer mr-4    bg-ssgreen `
