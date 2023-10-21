ff30e1c2a51a77cfa0b4ba78ff68d85da9b966de
ms29ash <ms29ash@gmail.com>
2023-10-21 18:35:46 +0530

    Fix position of product sidebar

--- src/Components/Products/ProductSideBar.js
+++ src/Components/Products/ProductSideBar.js
@@ -3,42 +3,44 @@ import tw from "tailwind-styled-components";
 
 function ProductSideBar() {
   return (
-    <div className="  min-w-[300px]  ">
-      <div className="fixed top-[15%] min-w-[300px] max-h-[80vh] overflow-y-hidden">
-        <h1 className=" pt-6 pb-4">Categories</h1>
-        <div className=" bg-white rounded-lg  h-[80vh] overflow-y-auto pb-[30%] ">
-          <Wrapper $selected={true}>
-            <Img src="/images/products/icons/Vegetable.jpg" alt="" />
-            <Head>Fresh Vegetables</Head>
-          </Wrapper>
-          <Wrapper>
-            <Img src="/images/products/icons/fruit.jpg" alt="" />
-            <Head>Fresh Fruits</Head>
-          </Wrapper>
-          <Wrapper>
-            <Img src="/images/products/icons/juice.jpg" alt="" />
-            <Head>Fresh Juice</Head>
-          </Wrapper>
-          <Wrapper>
-            <Img src="/images/products/icons/dairy.jpg" alt="" />
-            <Head>Dairy</Head>
-          </Wrapper>
-          <Wrapper>
-            <Img src="/images/products/icons/frozen.jpg" alt="" />
-            <Head>Frozen Veg</Head>
-          </Wrapper>
-          <Wrapper>
-            <Img src="/images/products/icons/sliced.jpg" alt="" />
-            <Head>Sliced Fruit</Head>
-          </Wrapper>
-          <Wrapper>
-            <Img src="/images/products/icons/herbs.jpg" alt="" />
-            <Head>Herbs & Leafies</Head>
-          </Wrapper>
-          <Wrapper>
-            <Img src="/images/products/icons/exotic.jpg" alt="" />
-            <Head>Exotics</Head>
-          </Wrapper>
+    <div className="  min-w-[300px] h-full overflow-y-hidden  ">
+      <div className="top-0 fixed bottom-0 flex items-center ">
+        <div className=" top-[15%] min-w-[300px] max-h-[80vh] overflow-y-hidden">
+          <h1 className=" pt-6 pb-4">Categories</h1>
+          <div className=" bg-white rounded-lg  h-[80vh] overflow-y-auto pb-[30%] ">
+            <Wrapper $selected={true}>
+              <Img src="/images/products/icons/Vegetable.jpg" alt="" />
+              <Head>Fresh Vegetables</Head>
+            </Wrapper>
+            <Wrapper>
+              <Img src="/images/products/icons/fruit.jpg" alt="" />
+              <Head>Fresh Fruits</Head>
+            </Wrapper>
+            <Wrapper>
+              <Img src="/images/products/icons/juice.jpg" alt="" />
+              <Head>Fresh Juice</Head>
+            </Wrapper>
+            <Wrapper>
+              <Img src="/images/products/icons/dairy.jpg" alt="" />
+              <Head>Dairy</Head>
+            </Wrapper>
+            <Wrapper>
+              <Img src="/images/products/icons/frozen.jpg" alt="" />
+              <Head>Frozen Veg</Head>
+            </Wrapper>
+            <Wrapper>
+              <Img src="/images/products/icons/sliced.jpg" alt="" />
+              <Head>Sliced Fruit</Head>
+            </Wrapper>
+            <Wrapper>
+              <Img src="/images/products/icons/herbs.jpg" alt="" />
+              <Head>Herbs & Leafies</Head>
+            </Wrapper>
+            <Wrapper>
+              <Img src="/images/products/icons/exotic.jpg" alt="" />
+              <Head>Exotics</Head>
+            </Wrapper>
+          </div>
         </div>
       </div>
     </div>
