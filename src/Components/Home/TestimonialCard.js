5e3a9b0e37cf3e2ea0e9160fbaef843f1fd711f0
ms29ash <ms29ash@gmail.com>
2023-10-22 08:18:35 +0530

    add data in reviews

--- src/Components/Home/TestimonialCard.js
+++ src/Components/Home/TestimonialCard.js
@@ -1,48 +1,45 @@
-import React from 'react'
+import React from "react";
 import tw from "tailwind-styled-components";
-import { BsArrowRightShort, BsArrowLeftShort } from 'react-icons/bs'
+import { BsArrowRightShort, BsArrowLeftShort } from "react-icons/bs";
 
-
-function TestimonialCard({ review, change }) {
-    return (
-        <Container>
-
-            <Card>
-                <Wrapper>
-                    <Head>{review?.name}</Head>
-                    <p className='leading-5 text-xs'>{review?.review}</p>
-                    <Buttons>
-                        <LeftBtn onClick={() => change(0)} >
-                            <BsArrowLeftShort />
-                        </LeftBtn>
-                        <RightBtn onClick={() => change(1)} >
-                            <BsArrowRightShort />
-                        </RightBtn>
-                    </Buttons>
-                </Wrapper>
-                <Box>
-                    <Img src="https://images.pexels.com/photos/793772/pexels-photo-793772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                </Box>
-
-            </Card>
-        </Container>
-    )
+function TestimonialCard({ review, next, previous }) {
+  return (
+    <Container>
+      <Card>
+        <Wrapper>
+          <Head>{review?.name}</Head>
+          <p className="leading-5 text-[0.9rem] text-gray-800 ">
+            {review?.content}
+          </p>
+          <Buttons>
+            <LeftBtn onClick={previous}>
+              <BsArrowLeftShort />
+            </LeftBtn>
+            <RightBtn onClick={next}>
+              <BsArrowRightShort />
+            </RightBtn>
+          </Buttons>
+        </Wrapper>
+        <Box>
+          <Img src={review?.image} alt="" />
+        </Box>
+      </Card>
+    </Container>
+  );
 }
 
-export default TestimonialCard
+export default TestimonialCard;
 
-const Container = tw.div` basis-[100vw] scroll-end scroll-mx-5 shrink-0 p-8 grid place-items-center snap-always snap-center`
+const Container = tw.div` basis-[100vw] scroll-end scroll-mx-5 shrink-0 p-8 grid place-items-center snap-always snap-center
 
-const Card = tw.div` h-full shadow-xl  w-[70%]  items-center bg-white flex `
-const Head = tw.h1`my-3`
-const Wrapper = tw.div`ml-[5%] w-[50%] mr-[3%] `;
-const Box = tw.div` w-[42%] `
-const Img = tw.img` aspect-[4/3] object-cover rounded bottom-6 left-6 relative shadow-xl  `
-const Buttons = tw.div`mt-4`
-const LeftBtn = tw.button`bg-ssorange hover:bg-ssgreen  text-white text-xl mr-2 p-2 rounded-full shadow-xl`
-const RightBtn = tw(LeftBtn)` bg-slate-200 hover:bg-slate-400 text-black`
+`;
 
-
-
-
-
+const Card = tw.div` h-full shadow-2xl w-full lg:w-[70%]  items-center bg-white  rounded-lg
+flex md:flex-row flex-col-reverse  `;
+const Head = tw.h1`my-3`;
+const Wrapper = tw.div`ml-[5%] w-full p-[5%] lg:px-[2.5%]  lg:w-[50%] mr-[3%] `;
+const Box = tw.div` w-full lg:w-[42%] `;
+const Img = tw.img` aspect-[4/3] object-cover rounded bottom-6 left-6 md:relative shadow-xl  `;
+const Buttons = tw.div`mt-4`;
+const LeftBtn = tw.button`bg-ssorange hover:bg-ssgreen  text-white text-xl mr-2 p-2 rounded-full shadow-xl`;
+const RightBtn = tw(LeftBtn)` bg-slate-200 hover:bg-slate-400 text-black`;
--- src/Components/Home/Testimonials.js
+++ src/Components/Home/Testimonials.js
@@ -2,48 +2,65 @@ import React, { useRef } from "react";
 import tw from "tailwind-styled-components";
 import "../../Styles/Testimonials.css";
 import TestimonialCard from "./TestimonialCard";
+import axios from "../../axios";
+import { useQuery } from "react-query";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 function Testimonials() {
-  const scrollRef = useRef()
-  const review = {
-    name: "Rohit Sharma",
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis officia ex consequuntur. Veritatis omnis nesciunt, corporis dignissimos sed non sequi voluptatem Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nobis quibusdam, maiores ut adipisci reprehenderit voluptatum soluta deserunt reiciendis, dolores inventore a?',
-    date: '26 Jul 2020',
-  }
+  const fetchFood = async () => {
+    return axios.get("/reviews");
+  };
 
-  const change = (n) => {
+  const { data } = useQuery("reviews", fetchFood);
 
-    if (n === 1) {
+  const sliderRef = useRef(null);
 
-      scrollRef.current.scrollLeft += window.innerWidth
-    } else {
-      scrollRef.current.scrollLeft -= window.innerWidth
+  const next = () => {
+    sliderRef.current.slickNext();
+  };
 
-    }
+  const previous = () => {
+    sliderRef.current.slickPrev();
+  };
 
-  }
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 500,
+    autoplaySpeed: 3000,
+  };
 
   return (
-    <Container className="testimonials-section" >
+    <Container className="testimonials-section">
       <Head>Reviews</Head>
       <Wrapper>
-        <Slider ref={scrollRef} >
-
-
-          <TestimonialCard change={change} review={review} />
-          <TestimonialCard change={change} review={review} />
-          <TestimonialCard change={change} review={review} />
-          <TestimonialCard change={change} review={review} />
-          <TestimonialCard change={change} review={review} />
-        </Slider>
+        {data?.data?.reviews && (
+          <Slider ref={sliderRef} {...settings}>
+            {data?.data?.reviews.map((review) => {
+              return (
+                <TestimonialCard
+                  key={review._id}
+                  next={next}
+                  previous={previous}
+                  review={review}
+                />
+              );
+            })}
+          </Slider>
+        )}
       </Wrapper>
     </Container>
-  )
+  );
 }
 
-export default Testimonials
+export default Testimonials;
 
-const Wrapper = tw.div`w-screen `
-const Slider = tw.div`slider   relative w-[98vw] mx-[1vw] flex items-stretch gap-6 snap-x snap-mandatory overflow-x-auto pb-4`
+const Wrapper = tw.div`w-screen `;
+// const Slider = tw(Slder)`slider   relative w-[98vw] mx-[1vw] flex items-stretch gap-6 snap-x snap-mandatory overflow-x-auto pb-4`;
 const Container = tw.div`relative flex flex-col items-center `;
 const Head = tw.h1``;
